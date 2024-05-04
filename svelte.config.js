3b3db6fb6b6a479a5ef78b6554b85f535feec1dd
zheglo <sergeyzheglo@gmail.com>
2024-05-04 11:57:26 +0300

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
+// import adapter from '@sveltejs/adapter-vercel';
 import UnoCSS from '@unocss/svelte-scoped/preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -25,7 +26,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			edge: false,
+       split: true
+		})
 	}
 };
 
